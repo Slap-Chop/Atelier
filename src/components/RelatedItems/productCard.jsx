ec1ceb41dac64923f4b89efc416280a164e89c1e
Nick Johnson <ntjohns10@gmail.com>
2022-07-07 16:31:14 -0700

    file skelaton progress

---
+++ src/components/RelatedItems/productCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var productCard = (props) => {
+
+
+  return
+      (
+        <div class="card">
+          <img src="" alt="IMAGE N/A"/>
+        </div>
+      )
+
+}
+
+export default productCard;
