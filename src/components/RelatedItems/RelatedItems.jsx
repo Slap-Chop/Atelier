d4b096de3b02d187f988b8cd52632748b744f7a0
Nick Johnson <ntjohns10@gmail.com>
2022-07-08 16:17:33 -0700

    added conditional rendering for Comparison Component in App.jsx

--- src/components/RelatedItems/RelatedItems.jsx
+++ src/components/RelatedItems/RelatedItems.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import config from '../../../config.js';
 
 class RelatedItems extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      ID: this.props.currentId
     }
   }
+
+
+
+  componentDidMount() {
+    console.log(this.props)
+
+    // console.log(ID)
+    axios.defaults.headers.common['Authorization'] = config.TOKEN;
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/:${this.props.currentId}`, {params: this.props.currentId})
+    .then((data) => console.log(data))
+  }
+
 
   render() {
     return(<div>
       RelatedItems!
-    </div>
-
+          </div>
     )
   }
 }
