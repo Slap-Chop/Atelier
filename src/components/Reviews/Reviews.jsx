82dbf2c42df16b2abf16d3be19fc2b90c196e4fc
Jonathan Oh <jonoh832@gmail.com>
2022-07-13 13:51:28 -0700

    Refactored to use hooks

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -5,84 +5,49 @@ import config from '../../../config.js';
 import ReviewsList from './ReviewsList.jsx';
 
 
-class Reviews extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      reviews: []
-    }
-    this.getReviews = this.getReviews.bind(this);
-  }
+export default function Reviews (props) {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //     reviews: []
+  //   }
+  //   this.getReviews = this.getReviews.bind(this);
+  // }
   // hooks need to be called in order and can't be put in a condiiton
+  const [productId, setProductId] = useState(props.id);
+  const [reviews, setReviews] = useState([]);
+  const [more, setMore] = useState(true);
+  const [reviewsToShow, setReviewsToShow] = useState(2);
 
-  // passing in the default state is
-
-  // runs the function only the very first time the page renders
-
-  // doing it this way will only run the function once
-  // const [count, setCount] = useState(() => {
-  componentDidMount() {
-    this.getReviews();
-  }
-
-  // });
-  getReviews = () => {
+  useEffect(() => {
     axios.defaults.headers.common['Authorization'] = config.TOKEN;
 
-    console.log('id', this.props.id);
+    console.log('id', productId);
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews', {
       params: {product_id: '40344'}
     })
     .then((response) => {
       console.log('reponse', response);
-      this.setState({reviews: response.data.results})
-      console.log('REVIEWS:', this.state.reviews);
+      setReviews(response.data.results);
+      console.log('REVIEWS:', reviews);
     })
     .catch((err) => console.log('err', err));
+  }, [productId]);
 
-  }
 
+  return (
+    <>
+      <div className="reviews">
+        Reviews:
+        <ReviewsList reviews={reviews} more={more} setMore={setMore} reviewsToShow={reviewsToShow}
+          setReviewsToShow={setReviewsToShow}
+        />
+      </div>
+    </>
+  )
 
-  // return (
-  //   <>
-  //     <button onClick={decrementCount}>-</button>
-  //     <span>{count}</span>
-  //     <button>+</button>
-  //   </>
-  // )
-
-  render() {
-    return (
-      <>
-        <div className="w3-container">
-          Reviews:
-          <ReviewsList reviews={this.state.reviews}/>
-        </div>
-
-      </>
-    )
-    }
 }
 
 /*
 Container must have max height
 */
-
-// class Reviews extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-
-//     }
-//   }
-
-//   render() {
-//     return(<div>
-//       Reviews!
-//     </div>
-
-//     )
-//   }
-// }
-
-export default Reviews;
--- src/components/Reviews/ReviewsList.jsx
+++ src/components/Reviews/ReviewsList.jsx
@@ -1,73 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReviewsListEntry from './ReviewsListEntry.jsx';
 
-class ReviewsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      more: true,
-      reviewsToShow: 2,
-      expanded: false
-    }
+export default function ReviewsList ({reviewsToShow, reviews, more, setMore, setReviewsToShow}) {
 
-    this.showMoreReviews = this.showMoreReviews.bind(this);
+  function showMoreReviews() {
+
+    let reviewsCount = reviewsToShow;
+    reviewsCount < reviews.length ? setReviewsToShow(reviewsCount +  2) : setMore(false);
+
+    console.log('NEW COUNT', reviewsToShow)
   }
 
-  showMoreReviews() {
-    // append to current reviews list
-    // add additional 2 more reviews
-    // let currentMoreVal = this.state.more;
-    // this.setState({more: currentMoreVal+1});
 
-    let reviewsCount = this.state.reviewsToShow;
-    reviewsCount < this.props.reviews.length ? this.setState({
-      reviewsToShow: (reviewsCount + 2)
-    }) : this.setState({more: false});
+  return (
+    <>
+    <label>Sort By</label>
+    <select>
+      <option value="test">TEST</option>
+    </select>
+    <label>Sort By</label>
+    <select>
+      <option value="test2">TEST2</option>
+    </select>
+    <div>
+      <ul>
+        {reviews.slice(0, reviewsToShow).map((review, i) => <ReviewsListEntry review={review} key={i}/>)}
+      </ul>
+    </div>
 
-    console.log('NEW COUNT', this.state.reviewsToShow)
-  }
-
-  // componentDidMount() {
-  //   this.setState;
-  // }
-
-  render() {
-    console.log('Reviews List props reviews', this.props.reviews)
-    return (
-      <>
-      <label>Sort By</label>
-      <select>
-        <option value="test">TEST</option>
-      </select>
-      <label>Sort By</label>
-      <select>
-        <option value="test2">TEST2</option>
-      </select>
-      <div>
-        <ul>
-          {this.props.reviews.slice(0, this.state.reviewsToShow).map((review, i) => <ReviewsListEntry review={review} key={i}/>)}
-        </ul>
-      </div>
-      {/* {this.props.reviews.length > 2 ? (this.props.reviews).map((review) =>
-        <ReviewsListEntry review={review}/>) : null} */}
-      {/* {this.props.reviews.length > 2 ?
-      <>
-        <ReviewsListEntry review={this.props.reviews[0]}/>
-        <ReviewsListEntry review={this.props.reviews[1]}/>
-      </> : null}
-
-      {this.state.more > 0 && this.state.more < 2 ?
-      <>
-        <ReviewsListEntry review={this.props.reviews[this.state.more+1]}/>
-        <ReviewsListEntry review={this.props.reviews[this.state.more+2]}/>
-      </> : null} */}
-
-      {this.state.more ? <button onClick={() => this.showMoreReviews()}>More Reviews</button> : <button disabled={true}>No more reviews</button>}
-      </>
-    )
-  }
+    {more ? <button onClick={() => showMoreReviews()}>More Reviews</button> : <button disabled={true}>No more reviews</button>}
+    </>
+  )
 
 }
-
-export default ReviewsList;
 
