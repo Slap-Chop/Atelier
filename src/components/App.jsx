27fe2e2bdfb2d1eb8f42bbf482f1cf837befaf54
Jmok19927 <jmok19927@gmail.com>
2022-07-19 14:47:00 -0700

    added details and info tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -141,6 +141,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.state.productList, this.state.currentId, this.state.currentProduct)
     return(<div>
       Hi friends!
       npm run react-dev should open a live listener of webpack,
